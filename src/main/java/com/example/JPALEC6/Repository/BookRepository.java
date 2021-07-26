dd4c2ee934d60585bf8979ba8f1651454c5e5f3b
Krish Murarka <64781854+krishmurarka@users.noreply.github.com>
2021-07-26 23:43:45 +0530

    updated_Book_Repository

--- src/main/java/com/example/JPALEC6/Repository/BookRepository.java
+++ src/main/java/com/example/JPALEC6/Repository/BookRepository.java
@@ -4,5 +4,9 @@ import com.example.JPALEC6.Models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book,Integer> {
-
+  
+     List<Book> findByAuthorName(String name);
+        List<Book>findByName(String name);
+        List<Book>findByCost(int cost);
+        Book findById(int id);
 }
