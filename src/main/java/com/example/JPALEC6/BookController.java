9ee33d39d73c0c1bf5d03748c685ac4da39d3a91
Krish Murarka <64781854+krishmurarka@users.noreply.github.com>
2021-07-26 23:46:30 +0530

    Update BookController.java

--- src/main/java/com/example/JPALEC6/BookController.java
+++ src/main/java/com/example/JPALEC6/BookController.java
@@ -1,4 +1,4 @@
-package com.example.JPALEC6.Models;
+package com.example.JPALEC6;
 
 import com.example.JPALEC6.Repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
